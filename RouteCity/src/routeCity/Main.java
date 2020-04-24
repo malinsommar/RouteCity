feeeb6c1dec97408238cf96b5f0912a5283e425e
96simben <simoncraft3@hotmail.com>
2020-04-24 14:43:57 +0200

    out fix

--- RouteCity/src/routeCity/Main.java
+++
@@ -1,9 +0,0 @@
-package routeCity;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hey");
-        StringCity stringcity = new StringCity();
-        stringcity.stringRoads();
-    }
-}
---
+++ src/routeCity/Main.java
@@ -0,0 +1,9 @@
+package routeCity;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hey");
+        StringCity stringcity = new StringCity();
+        stringcity.stringRoads();
+    }
+}
